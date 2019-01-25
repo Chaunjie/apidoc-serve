937821b3118327d01305858b15acbecaae9c5a25
chaunjie <894576801@qq.com>
2019-01-25 17:29:33 +0800

    #0: update gitignore

--- src/db/index.js
+++ src/db/index.js
@@ -2,7 +2,7 @@ import { db } from 'nodejs-mysql-model'
 
 export default function Db () {
   db.createPool({
-    host: '106.12.85.98', // localhost
+    host: 'localhost', // localhost
     user: 'root',
     password: 'root', // qwer123456
     database: 'apidoc',
